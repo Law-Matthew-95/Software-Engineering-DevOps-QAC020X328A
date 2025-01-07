18e8fdda53c21180f3437b7166de2cf0cf0c9bc9
Matthew Law <law.matthew1995@gmail.com>
2025-01-07 11:54:55 +0000

    Added X-Content-Type-Options header to main.py

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ app = create_app()
 @app.after_request
 def after_request(response):
     response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    response.headers['X-Content-Type-Options'] = 'nosniff'
     return response
+
 
 if __name__ == '__main__':
     app.run(debug=True)
