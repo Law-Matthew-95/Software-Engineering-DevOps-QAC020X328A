852a694bc39ef7f052186b203e2b31bad4eb1ea9
Matthew Law <law.matthew1995@gmail.com>
2025-01-07 12:35:12 +0000

    Refactor Content-Security-Policy header to a single line format for improved readability

--- main.py
+++ main.py
@@ -8,12 +8,7 @@ def after_request(response):
     response.headers['X-Frame-Options'] = 'SAMEORIGIN'
     response.headers['X-Content-Type-Options'] = 'nosniff'
     response.headers['X-XSS-Protection'] = '1; mode=block'
-    response.headers['Content-Security-Policy'] = """
-    default-src 'self'; 
-    style-src 'self' https://cdn.jsdelivr.net https://stackpath.bootstrapcdn.com; 
-    script-src 'self';
-    font-src 'self' https://cdn.jsdelivr.net https://stackpath.bootstrapcdn.com;
-    """
+    response.headers['Content-Security-Policy'] = "default-src 'self'; style-src 'self' https://cdn.jsdelivr.net https://stackpath.bootstrapcdn.com; script-src 'self'; font-src 'self' https://cdn.jsdelivr.net https://stackpath.bootstrapcdn.com;"
     response.headers['Referrer-Policy'] = 'no-referrer'
     response.headers['Permissions-Policy'] = "geolocation=(), microphone=(), camera=()"
     response.headers['strict-transport-security'] = 'max-age=31536000; includeSubDomains'
