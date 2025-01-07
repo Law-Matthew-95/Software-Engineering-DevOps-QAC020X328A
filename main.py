11e97811470d8acb6221f083f08cae12869c7104
Matthew Law <law.matthew1995@gmail.com>
2025-01-07 10:32:45 +0000

    Added x-frame to main.py

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ from website import create_app
 
 app = create_app()
 
+@app.after_request
+def after_request(response):
+    response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    return response
+
 if __name__ == '__main__':
     app.run(debug=True)
